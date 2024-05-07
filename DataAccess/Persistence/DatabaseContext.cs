ecdde5265d212c47f38e75629e481be579a06c80
pasko <stipandzijapasko@gmail.com>
2024-05-07 19:05:54 +0200

    DBcontext update

--- DataAccess/Persistence/DatabaseContext.cs
+++ DataAccess/Persistence/DatabaseContext.cs
@@ -1,55 +1,82 @@
-﻿
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Core.Entiteti;
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
-using Microsoft.EntityFrameworkCore.Metadata;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DataAccess.Persistence
 {
     public class DatabaseContext : DbContext
     {
         private readonly IConfiguration configuration;
-        //public DatabaseContext(DbContextOptions<DatabaseContext> options, IConfiguration configuration)
-        //    : base(options)
-        //{
-        //    this.configuration = configuration;
-        //}
+
+        // Uncomment if needed
+        // public DatabaseContext(DbContextOptions<DatabaseContext> options, IConfiguration configuration)
+        //     : base(options)
+        // {
+        //     this.configuration = configuration;
+        // }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-DMLS484\\MSSQLSERVER01;Initial Catalog=ShakFoodDb;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
         }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-            modelBuilder.Entity<Restaurant_Food>()
-                .HasKey(rf => new { rf.Food_ID, rf.Restaurant_ID });
+            modelBuilder.Entity<City>(entity =>
+            {
+                entity.ToTable("City");
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Name).IsRequired().HasMaxLength(100);
+                entity.Property(c => c.ZIP).IsRequired().HasMaxLength(10);
+                entity.Property(c => c.County).HasMaxLength(100);
+                entity.Property(c => c.City_code).HasMaxLength(10);
+            });
 
-            modelBuilder.Entity<Restaurant_Food>()
-                .HasOne(rf => rf.Food)
-                .WithMany()
-                .HasForeignKey(rf => rf.Food_ID);
+            modelBuilder.Entity<Food_category>(entity =>
+            {
+                entity.ToTable("Food_category");
+                entity.HasKey(fc => fc.Id);
+                entity.Property(fc => fc.Category).IsRequired().HasMaxLength(100);
+            });
 
-            modelBuilder.Entity<Restaurant_Food>()
-                .HasOne(rf => rf.Restaurant)
-                .WithMany()
-                .HasForeignKey(rf => rf.Restaurant_ID);
+            modelBuilder.Entity<Food>(entity =>
+            {
+                entity.ToTable("Food");
+                entity.HasKey(f => f.Id);
+                entity.Property(f => f.Name).IsRequired().HasMaxLength(100);
+                entity.Property(f => f.Price).IsRequired().HasMaxLength(10);
+                entity.Property(f => f.Description).HasMaxLength(100);
+                entity.HasOne(f => f.Category_ID).WithMany().HasForeignKey("Category_ID");
+            });
+
+            modelBuilder.Entity<Restaurant>(entity =>
+            {
+                entity.ToTable("Restaurant");
+                entity.HasKey(r => r.Id);
+                entity.Property(r => r.Name).IsRequired().HasMaxLength(100);
+                entity.HasOne(r => r.City).WithMany().HasForeignKey("City_ID");
+            });
+
+            modelBuilder.Entity<Restaurant_Food>(entity =>
+            {
+                entity.ToTable("Restaurant_Food");
+                entity.HasKey(rf => new { rf.Food_ID, rf.Restaurant_ID });
+                entity.HasOne(rf => rf.Food).WithMany().HasForeignKey(rf => rf.Food_ID);
+                entity.HasOne(rf => rf.Restaurant).WithMany().HasForeignKey(rf => rf.Restaurant_ID);
+            });
         }
+
         public DbSet<City> City { get; set; }
         public DbSet<Food> Food { get; set; }
         public DbSet<Food_category> Food_category { get; set; }
         public DbSet<Restaurant> Restaurant { get; set; }
         public DbSet<Restaurant_Food> Restaurant_Food { get; set; }
-       
     }
-   
 }
--- FoodRecommendation-main/FoodRecommendation-main/Core/Entities/Food_category.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Common;
-
-namespace Core.Entiteti
-{
-    public class Food_category : BaseEntity
-    {
-        public string Category { get; set; }
-    }
-}
