abd24225cad1b3d458cc8ad90f23690150bb6821
pasko <stipandzijapasko@gmail.com>
2024-05-07 18:31:56 +0200

    DBcontext update

---
+++ FoodRecommendation-main/FoodRecommendation-main/Core/Entities/Restaurant_Food.cs
@@ -0,0 +1,12 @@
+﻿using Core.Common;
+namespace Core.Entiteti
+{
+    public class Restaurant_Food
+    {
+        public Guid Food_ID;
+        public Guid Restaurant_ID;
+
+        public Food Food { get; set; }
+        public Restaurant Restaurant { get; set;}
+    }
+}
